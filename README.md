045cd4e3c317a7c6015bbfa0fc29603c184d7907
Jeremy Fleischman <jeremyfleischman@gmail.com>
2012-05-16 22:13:01 -0700

    Updated setup.py with proper dependencies.

---
+++ README.md
@@ -0,0 +1,10 @@
+# Setup
+## Clone & cd into git repo
+
+## Set up virtualenv & activate virtualenv
+1. `curl -O https://raw.github.com/pypa/virtualenv/master/virtualenv.py`
+2. `python virtualenv.py webkit_remote_debugger_env`
+3. `. webkit_remote_debugger_env/bin/activate`
+
+## Install requirements
+1. `pip install -e .`
--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@
 from distutils.core import setup
 
 setup(name='webkit_remote_debugger',
-      version='.1',
-      description='',
+      version='0.1',
       url='https://github.com/jfly/webkit_remote_debugger',
       packages=['webkit_remote_debugger'],
+
+      install_requires=["httplib2>=0.7.4", "ws4py>=0.2.1"]
      )
